b46a42566975b95c147b523bb8f2f6bd5b12e987
adam <adamjleung123@gmail.com>
2021-09-11 19:32:53 +1000

    Basic leaderboard table, typing, css

--- pages/components/RightPanel.tsx
+++ pages/components/RightPanel.tsx
@@ -1,3 +1,36 @@
-const RightPanel = () => {};
+import Leaderboard from "./Leaderboard";
+import styles from "../../styles/right-panel.module.css";
+
+import Box from "@material-ui/core/Box";
+
+const RightPanel = () => {
+  return (
+    <Box className={styles.panel}>
+      <Leaderboard
+        users={[
+          { id: 123, mmr: 1000, name: "Asdfsa" },
+          { id: 123, mmr: 1000, name: "Asdfsa" },
+          { id: 123, mmr: 1000, name: "Asdfsa" },
+          { id: 123, mmr: 1000, name: "Asdfsa" },
+          { id: 123, mmr: 1000, name: "Asdfsa" },
+          { id: 123, mmr: 1000, name: "Asdfsa" },
+          { id: 123, mmr: 1000, name: "Asdfsa" },
+          { id: 123, mmr: 1000, name: "Asdfsa" },
+          { id: 123, mmr: 1000, name: "Asdfsa" },
+          { id: 123, mmr: 1000, name: "Asdfsa" },
+          { id: 123, mmr: 1000, name: "Asdfsa" },
+          { id: 123, mmr: 1000, name: "Asdfsa" },
+          { id: 123, mmr: 1000, name: "Asdfsa" },
+          { id: 123, mmr: 1000, name: "Asdfsa" },
+          { id: 123, mmr: 1000, name: "Asdfsa" },
+          { id: 123, mmr: 1000, name: "Asdfsa" },
+          { id: 123, mmr: 1000, name: "Asdfsa" },
+          { id: 123, mmr: 1000, name: "Asdfsa" },
+          { id: 123, mmr: 1000, name: "Asdfsa" },
+        ]}
+      />
+    </Box>
+  );
+};
 
 export default RightPanel;
