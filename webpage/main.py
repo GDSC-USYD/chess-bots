f355d8ee7e4ae0cf8944e26d691a4d1dc3d933cb
SidneyRadwan <62342160+SidneyRadwan@users.noreply.github.com>
2021-09-14 01:40:10 +1000

    fix type error

--- webpage/main.py
+++ webpage/main.py
@@ -10,12 +10,15 @@ def hello_world():
     name = os.environ.get("NAME", "World")
 
     works = False
-    db_user, db_pass, db_name, db_host = None
+    db_user = None
+    db_pass = None
+    db_name = None
+    db_host = None
     db_user = os.environ["DB_USER"]
     db_pass = os.environ["DB_PASS"]
     db_name = os.environ["DB_NAME"]
     db_host = os.environ["DB_NAME"]
-    if db_user is not None and db_pass is not None and db_name is not None and db_host is not None:
+    if None not in [db_user,db_pass,db_name,db_host]:
         works = True
     return f"Hello {name}! Works: {works}"
 
