77675457ad992bec8c35e4ac565970572b42380e
SidneyRadwan <62342160+SidneyRadwan@users.noreply.github.com>
2021-09-14 03:47:59 +1000

    unix socket test

--- webpage/main.py
+++ webpage/main.py
@@ -1,31 +1,62 @@
 import os
 import mysql.connector
+import sqlalchemy
+
 
 from flask import Flask
 
 app = Flask(__name__)
 
+def init_connection_engine():
+    db_config = {
+        "pool_size": 5,
+        "max_overflow": 2,
+        "pool_timeout": 30,  # 30 seconds
+        "pool_recycle": 1800,  # 30 minutes
+    }
+    return init_unix_connection_engine(db_config)
+
+
+def init_unix_connection_engine(db_config):
+    # [START cloud_sql_mysql_sqlalchemy_create_socket]
+    # Remember - storing secrets in plaintext is potentially unsafe. Consider using
+    # something like https://cloud.google.com/secret-manager/docs/overview to help keep
+    # secrets secret.
+    db_user = os.environ["DB_USER"]
+    db_pass = os.environ["DB_PASS"]
+    db_name = os.environ["DB_NAME"]
+    db_socket_dir = os.environ.get("DB_SOCKET_DIR", "/cloudsql")
+    cloud_sql_connection_name = os.environ["CLOUD_SQL_CONNECTION_NAME"]
+
+    pool = sqlalchemy.create_engine(
+        # Equivalent URL:
+        # mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=<socket_path>/<cloud_sql_instance_name>
+        sqlalchemy.engine.url.URL.create(
+            drivername="mysql+pymysql",
+            username=db_user,  # e.g. "my-database-user"
+            password=db_pass,  # e.g. "my-database-password"
+            database=db_name,  # e.g. "my-database-name"
+            query={
+                "unix_socket": "{}/{}".format(
+                    db_socket_dir,  # e.g. "/cloudsql"
+                    cloud_sql_connection_name)  # i.e "<PROJECT-NAME>:<INSTANCE-REGION>:<INSTANCE-NAME>"
+            }
+        ),
+        **db_config
+    )
+
+    return pool
 
 @app.route("/")
 def hello_world():
     name = os.environ.get("NAME", "World")
 
-    db_user = os.environ["DB_USER"]
-    db_pass = os.environ["DB_PASS"]
-    db_host = os.environ["DB_HOST"]
-    db_name = os.environ["DB_NAME"]
+    db = init_connection_engine()
 
-    # Extract host and port from db_host
-    host_args = db_host.split(":")
-    db_hostname, db_port = host_args[0], int(host_args[1])
+    with db.connect() as conn:
+        conn.execute( "SELECT * FROM players")
 
-    mydb = mysql.connector.connect(host=db_hostname, port=db_port, user=db_user, password=db_pass, database=db_name)
-
-    mycursor = mydb.cursor()
-
-    mycursor.execute("SELECT * FROM players")
-
-    myresult = mycursor.fetchall()
+    myresult = conn.fetchall()
 
     html_string = f"<html><body><h1>Hello {name}! Works: {works}</h1>"
 
