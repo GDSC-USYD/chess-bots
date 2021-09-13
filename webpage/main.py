1524ae17987ef677dd8ba9dbb2ec24710a4ba3bc
SidneyRadwan <62342160+SidneyRadwan@users.noreply.github.com>
2021-09-14 01:26:36 +1000

    db env var test

--- webpage/main.py
+++ webpage/main.py
@@ -8,7 +8,16 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     name = os.environ.get("NAME", "World")
-    return "Hello {}!".format(name)
+
+    works = False
+    db_user, db_pass, db_name db_host = None
+    db_user = os.environ["DB_USER"]
+    db_pass = os.environ["DB_PASS"]
+    db_name = os.environ["DB_NAME"]
+    db_host = os.environ["DB_NAME"]
+    if db_user is not None and db_pass is not None and db_name is not None and db_host is not None:
+        works = True
+    return f"Hello {name}! Works: {works}"
 
 
 if __name__ == "__main__":
