12a1dd1759bbc9dcbcb1b2adb924c4a14d853e71
SidneyRadwan <62342160+SidneyRadwan@users.noreply.github.com>
2021-09-14 02:07:03 +1000

    db query test

--- webpage/main.py
+++ webpage/main.py
@@ -1,4 +1,5 @@
 import os
+import mysql.connector
 
 from flask import Flask
 
@@ -9,18 +10,30 @@ app = Flask(__name__)
 def hello_world():
     name = os.environ.get("NAME", "World")
 
-    works = False
-    db_user = None
-    db_pass = None
-    db_name = None
-    db_host = None
     db_user = os.environ["DB_USER"]
     db_pass = os.environ["DB_PASS"]
-    db_name = os.environ["DB_NAME"]
     db_host = os.environ["DB_NAME"]
-    if None not in [db_user,db_pass,db_name,db_host]:
-        works = True
-    return f"Hello {name}! Works: {works}"
+    db_name = os.environ["DB_NAME"]
+
+    cnx = mysql.connector.connect(user=db_user, password=db_pass,
+                                  host=db_host,
+                                  database=db_name)
+
+
+    mycursor = mydb.cursor()
+
+    mycursor.execute("SELECT * FROM players")
+
+    myresult = mycursor.fetchall()
+
+    html_string = f"<h>Hello {name}! Works: {works}</h>"
+
+    for x in myresult:
+      html_string += f"<\p>{x}\n<\p>"
+
+    cnx.close()
+
+    return html_string
 
 
 if __name__ == "__main__":
