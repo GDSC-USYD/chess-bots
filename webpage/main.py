37aef8d08780e65b343dae7429dcc89dd444fda9
SidneyRadwan <62342160+SidneyRadwan@users.noreply.github.com>
2021-09-15 01:54:27 +1000

    unix socket fix test

--- webpage/main.py
+++ webpage/main.py
@@ -1,68 +1,44 @@
 import os
 import mysql.connector
-import sqlalchemy
-
 
 from flask import Flask
 
 app = Flask(__name__)
 
-def init_connection_engine():
-    db_config = {
-        "pool_size": 5,
-        "max_overflow": 2,
-        "pool_timeout": 30,  # 30 seconds
-        "pool_recycle": 1800,  # 30 minutes
-    }
-    return init_unix_connection_engine(db_config)
+
+def unix_connect_db(db_user, db_pass, db_name, db_socket_dir, cloud_sql_connection_name):
+
+    conn = mysql.connector.connect(user=db_user,
+                                password=db_pass,
+                                unix_socket="{}/{}".format(db_socket_dir, cloud_sql_connection_name),
+                                database=db_name)
+
+    return conn
 
 
-def init_unix_connection_engine(db_config):
-
-    db_user = os.environ["DB_USER"]
-    db_pass = os.environ["DB_PASS"]
-    db_name = os.environ["DB_NAME"]
-    db_socket_dir = os.environ.get("DB_SOCKET_DIR", "/cloudsql")
-    cloud_sql_connection_name = os.environ["CLOUD_SQL_CONNECTION_NAME"]
-
-    pool = sqlalchemy.create_engine(
-        # Equivalent URL:
-        # mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=<socket_path>/<cloud_sql_instance_name>
-        sqlalchemy.engine.url.URL.create(
-            drivername="mysql+pymysql",
-            username=db_user,  # e.g. "my-database-user"
-            password=db_pass,  # e.g. "my-database-password"
-            database=db_name,  # e.g. "my-database-name"
-            query={
-                "unix_socket": "{}/{}".format(
-                    db_socket_dir,  # e.g. "/cloudsql"
-                    cloud_sql_connection_name)  # i.e "<PROJECT-NAME>:<INSTANCE-REGION>:<INSTANCE-NAME>"
-            }
-        )
-    )
-
-    return pool
 
 @app.route("/")
 def hello_world():
     name = os.environ.get("NAME", "World")
+    db_user = os.environ["DB_USER"]
+    db_pass = os.environ["DB_PASS"]
+    db_name = os.environ["DB_NAME"]
+    db_socket_dir = "/cloudsql"
+    cloud_sql_connection_name = os.environ["CLOUD_SQL_CONNECTION_NAME"]
 
-    db = init_connection_engine()
+    conn = unix_connect_db(db_user, db_pass, db_name, db_socket_dir, cloud_sql_connection_name)
 
-    with db.connect() as conn:
-        conn.execute( "SELECT * FROM players")
+    conn.execute( "SELECT * FROM players")
 
     myresult = conn.fetchall()
 
-    html_string = f"<html><body><h1>Hello {name}! Works: {works}</h1>"
-
     for x in myresult:
-      html_string += f"<p>{x}/n</p>"
+        html_string += f"<p>{x}/n</p>"
 
 
     html_string += "</body></html>"
 
-    cnx.close()
+    conn.close()
 
     return html_string
 
