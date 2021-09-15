6dab7c743287d6ec17a8382c30e2e1108163c1e7
adam <adamjleung123@gmail.com>
2021-09-15 20:00:33 +1000

    Topbar and logo

--- pages/components/LeftPanel.tsx
+++ pages/components/LeftPanel.tsx
@@ -1,4 +1,5 @@
 import Leaderboard from "./Leaderboard";
+import TopBar from "./TopBar";
 import styles from "../../styles/left-panel.module.css";
 
 import Box from "@material-ui/core/Box";
@@ -6,6 +7,7 @@ import Box from "@material-ui/core/Box";
 const LeftPanel = () => {
   return (
     <Box className={styles.panel}>
+      <TopBar />
       <Leaderboard
         users={[
           { id: 123, mmr: 1000, name: "Asdfsa" },
