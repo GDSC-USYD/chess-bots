b18d7212d7ec01cc8200d05bd66940335a31b0c7
SidneyRadwan <62342160+SidneyRadwan@users.noreply.github.com>
2021-09-19 00:01:03 +1000

    routes 99% completed

--- backend/db_access.py
+++ backend/db_access.py
@@ -7,15 +7,6 @@ import re
 
 
 #NOT FINISHED
-def db_latest_batch_id(conn):
-    # retrieves and returns latest batch_id from db or None if no batches found
-    temp = None
-    batch_id = temp
-    return batch_id
-
-
-
-#NOT FINISHED
 def db_update_player_data(conn, player):
     #updates player data in db according to given player object
     pass
@@ -28,29 +19,60 @@ def db_insert_matches_data(conn, batch_id, matches_data):
     pass
 
 
-#NOT FINISHED
+
+def db_latest_batch_id(conn):
+    # retrieves and returns latest batch_id from db or None if no batches found
+
+    db_query = conn.execute(
+        f"SELECT MAX(batch_id) FROM matches;"
+    ).fetchall()
+
+    batch_id = db_query[0][0] # None if no batches found
+    #
+    # if batch_id = db_query[0][0] !=  0:
+    #     print(db_query)
+    #
+    #     print(batch_id)
+    #     return batch_id
+    # else:
+    return batch_id
+
+
+
 def db_insert_new_player(conn, table_name, name, password, email):
     # inserts given  into database of given player_id's
     # returns db_upload_message string -> status of db update
-    status_flag = 0 # just initialised flag
+    status_flag = 0 # just initialised status code
+    db_upload_message = "OK"
 
-    if check_valid_email(email) == True:
-        db_upload_message = "Not Implemented Yet"
+    # insert new player into db
+    try:
+        #conn.begin()
+        conn.execute(f"INSERT INTO {table_name} (name, status_flag, password, email) VALUES ('{name}', {status_flag}, '{password}', '{email}');")
+        #conn.commit()
+        return db_upload_message
+    except Exception as e:
+        print(e)
+        db_upload_message = e
+        return db_upload_message
 
-        # insert new player into db
-
-    else:
-        db_upload_message = "Invalid Email."
-
-    return db_upload_message
 
 
-#NOT FINISHED
 def db_confirm_player_credentials(conn, table_name, name, password):
-    # checks given  into database of given player_id's
+    # checks given into database of given player_id's
     # returns db_upload_message string -> status of db update
-    db_check_message = "Not Implemented Yet"
-    return db_check_message
+    db_check_message = "OK"
+
+    db_entry = conn.execute(
+        f"SELECT * FROM {table_name} WHERE name = '{name}' AND password = '{password}'"
+    ).fetchall()
+
+
+    if len(db_entry) > 1: #empty list if not found
+        return db_check_message
+    else: # not found
+        db_check_message = "Player not found."
+        return db_check_message
 
 
 
@@ -73,10 +95,10 @@ def db_retrieve_table_dict(conn, table_name):
 
 def db_retrieve_entry_data(conn, table_name, id_name, id_value):
     # calls db and returns table entry from given table with var_name = var_value
-    # returns tuple -> (id, ...entry data...)
+    # returns tuple -> (id, ...entry data...) or None if not found
 
     db_entry = conn.execute(
-        f"SELECT * FROM {table_name} WHERE {id_name}={id_value}"
+        f"SELECT * FROM {table_name} WHERE {id_name}='{id_value}';"
     ).fetchall()
 
     if len(db_entry) > 0:
@@ -91,7 +113,7 @@ def db_retrieve_table_data(conn, table_name):
     # returns tuples -> ((id, ...entry data...),...)
 
     db_table = conn.execute(
-        f"SELECT * FROM {table_name}"
+        f"SELECT * FROM {table_name};"
     ).fetchall()
 
     return db_table
@@ -103,7 +125,7 @@ def db_describe_table(conn, table_name):
     # returns tuples -> ((coloumn_var, ...coloum settings...),...)
 
     db_table_description = conn.execute(
-        f"DESCRIBE {table_name}"
+        f"DESCRIBE {table_name};"
     ).fetchall()
 
     if len(db_table_description) > 0:
@@ -119,7 +141,7 @@ def db_update_coloumn(conn, table_name, id_name, id_value, var_name, var_value):
     db_upload_message = "OK"
 
     db_entry = conn.execute(
-        f"SELECT * FROM {table_name} WHERE {id_name}={id_value}"
+        f"SELECT * FROM {table_name} WHERE {id_name}={id_value};"
     ).fetchall()
 
     if len(db_entry) > 0:
