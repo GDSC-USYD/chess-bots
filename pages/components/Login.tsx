f61a05f367c682850efd19141e1cf1e001b42b8d
adam <adamjleung123@gmail.com>
2021-09-20 23:08:56 +1000

    Password resestting tab

--- pages/components/Login.tsx
+++ pages/components/Login.tsx
@@ -11,6 +11,7 @@ type Input = { value: string; error?: string };
 enum Screen {
   LOGIN,
   SIGNUP,
+  FORGOT,
 }
 
 const Login = () => {
@@ -19,6 +20,8 @@ const Login = () => {
   const [username, setUsername] = useState<Input>({ value: "" });
 
   const [alert, setAlert] = useState<boolean>(false);
+  const [alertMessage, setAlertMessage] = useState<string>("");
+
   const [screen, setScreen] = useState<Screen>(Screen.LOGIN);
 
   useEffect(() => {
@@ -70,6 +73,32 @@ const Login = () => {
       setUsername({ value: "" });
 
       setAlert(true);
+      setAlertMessage(
+        "Succesfully registered! Check your email for a confirmation message"
+      );
+      setScreen(Screen.LOGIN);
+    }
+  };
+
+  const reset = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!email.value)
+      setEmail((email) => {
+        return { ...email, error: "Please enter an email address" };
+      });
+
+    if (!username.value)
+      setUsername((username) => {
+        return { ...username, error: "Please enter a username" };
+      });
+
+    if (email.value && username.value) {
+      setEmail({ value: "" });
+      setUsername({ value: "" });
+
+      setAlert(true);
+      setAlertMessage("Password reset email sent!");
       setScreen(Screen.LOGIN);
     }
   };
@@ -101,9 +130,18 @@ const Login = () => {
               variant="contained"
               color="primary"
               className={styles.button}
-              style={{ margin: "2rem 0" }}
+              style={{ marginTop: "2rem" }}
             >
               Sign In
+            </Button>
+            <Button
+              variant="contained"
+              color="secondary"
+              className={styles.button}
+              style={{ margin: "1rem 0 4rem" }}
+              onClick={() => setScreen(Screen.FORGOT)}
+            >
+              Forgot Password
             </Button>
           </form>
 
@@ -111,13 +149,13 @@ const Login = () => {
             variant="contained"
             color="secondary"
             className={styles.button}
-            style={{ marginTop: "5rem 0", backgroundColor: "#EA8600" }}
+            style={{ backgroundColor: "#EA8600" }}
             onClick={() => setScreen(Screen.SIGNUP)}
           >
             Create an Account
           </Button>
         </>
-      ) : (
+      ) : screen === Screen.SIGNUP ? (
         <>
           <form className={styles.form} onSubmit={signUp}>
             <TextField
@@ -167,6 +205,47 @@ const Login = () => {
             Cancel
           </Button>
         </>
+      ) : (
+        <>
+          <form className={styles.form} onSubmit={reset}>
+            <TextField
+              label="Username"
+              type="text"
+              error={username.error !== undefined}
+              helperText={username.error}
+              value={username.value}
+              onChange={(e) => setUsername({ value: e.target.value })}
+            />
+            <div style={{ padding: "10px 0" }} />
+            <TextField
+              label="Email"
+              type="email"
+              error={email.error !== undefined}
+              helperText={email.error}
+              value={email.value}
+              onChange={(e) => setEmail({ value: e.target.value })}
+            />
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              className={styles.button}
+              style={{ margin: "2rem 0" }}
+            >
+              Reset Password
+            </Button>
+          </form>
+
+          <Button
+            variant="contained"
+            color="secondary"
+            className={styles.button}
+            style={{ marginTop: "5rem 0" }}
+            onClick={() => setScreen(Screen.LOGIN)}
+          >
+            Cancel
+          </Button>
+        </>
       )}
       {alert && (
         <Snackbar
@@ -174,8 +253,13 @@ const Login = () => {
           autoHideDuration={6000}
           onClose={() => setAlert(false)}
         >
-          <Alert onClose={() => setAlert(false)}>
-            Succesfully registered! Check your email for a confirmation message
+          <Alert
+            onClose={() => {
+              setAlert(false);
+              setAlertMessage("");
+            }}
+          >
+            {alertMessage}
           </Alert>
         </Snackbar>
       )}
