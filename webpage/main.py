47da0939b7caa572649243e454515b3b5fe169dc
SidneyRadwan <62342160+SidneyRadwan@users.noreply.github.com>
2021-09-27 00:16:37 +1000

    comment out gamemaster

--- webpage/main.py
+++ webpage/main.py
@@ -1,7 +1,7 @@
 from db_connect import *
 from db_access import *
 from secure import *
-from game_master import *
+#from game_master import *
 #from send_email import * # email function added soon
 
 
@@ -360,9 +360,11 @@ def launch_chess_game_master():
     try:
         db = connect_to_db()
         with db.connect() as conn:
-            chess_game_master = ChessGameMaster(conn)
 
-            launch_status = chess_game_master.run()
+
+            #chess_game_master = ChessGameMaster(conn)
+
+            #launch_status = chess_game_master.run()
             #threading.Thread(target=chess_game_master.run).start()
 
     except Exception as e:
